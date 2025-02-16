5e2dccaac8d6e138e60ced68a509aae7389493ed
Marijn de Mul <106752077+Marijn-de-Mul@users.noreply.github.com>
2025-02-16 15:44:39 +0100

    Backend -> Host Change

--- Backend/app.py
+++ Backend/app.py
@@ -313,4 +313,4 @@ def calculate_distance(lat1, lon1, lat2, lon2):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(host='0.0.0.0', debug=True)
